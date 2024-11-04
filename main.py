a74ff56ff65e5769dca05827a9fb8f2c1fcac701
JumanjiJob <35mirzhan@mail.ru>
2024-11-04 16:05:40 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import requests
 
+response = requests.get('https://www.google.com/search?q=%D0%B3%D1%83%D0%B3%D0%BB+%D0%B4%D0%B8%D1%81%D0%BA&oq=&gs_lcrp=EgZjaHJvbWUqCQgAECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIPCAMQLhgDGI8BGLQCGOoCMhEIBBAAGAMYQhiPARi0AhjqAjIRCAUQABgDGEIYjwEYtAIY6gIyEQgGEAAYAxhCGI8BGLQCGOoCMg8IBxAuGAMYjwEYtAIY6gLSAQkxNTIwajBqMTWoAgiwAgE&sourceid=chrome&ie=UTF-8')
+print(response.status_code)
+
+# is response.ok:
+#     print("Запрос успешно выполнен")
+# else:
+#     ("произошла ошибка")
+
+print(response.content)
